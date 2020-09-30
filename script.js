c943febfaf99d9325c1b0247bc6656819699c8c7
noracurcio <noracurcio@gmail.com>
2020-09-30 13:04:13 -0600

    Added noted and got button to display content.

--- script.js
+++ script.js
@@ -10,51 +10,70 @@ var submitName = document.getElementById("submitName");
 var index = 0
 var time = 60
 
+
+//An object that holds all of the questions and answers in the quiz.
 var questionsArray = [
     {
-        questionTitle: "test question",
+        questionTitle: "Question 1",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         correctAnswer: "answer1"
     }, {
-        questionTitle: "test question",
+        questionTitle: "Question 2",
         choices: ["answer1", "answer2", "answer3", "answer4"],
-        correctAnswer: "answer1"
+        correctAnswer: "answer2"
     }, {
-        questionTitle: "test question",
+        questionTitle: "Question 3",
         choices: ["answer1", "answer2", "answer3", "answer4"],
-        correctAnswer: "answer1"
+        correctAnswer: "answer3"
     }, {
-        questionTitle: "test question",
+        questionTitle: "Question 4",
         choices: ["answer1", "answer2", "answer3", "answer4"],
-        correctAnswer: "answer1"
+        correctAnswer: "answer4"
     } 
     
 ]
 
+//Need to add event listeners on every button
+
+
+
+//a function that starts the quiz by removing the attribute "hidden"
 function start() {
     console.log("test")
     startScreen.setAttribute("class", "hidden")
     questions.removeAttribute("class")
-    setInterval(time,1000)
-    time.textContent = time
+    // setInterval(timer,1000)
+    timer.textContent = time
     getQuestion()
 }
 
+//my time counter
 function timer(){
     time --
     timer.textContent = time
     
 }
 
+//a function that gets the next question in the array 
 function getQuestion(){
     var currentQ = questionsArray[index]
+    console.log(currentQ)
     title.textContent = currentQ.questionTitle
     currentQ.choices.forEach(function(choice,i){
         console.log(choice,i)
         var button = document.createElement("button")
+        button.textContent = choice
         choices.appendChild(button)
     })
 }
+
+
+
+// event listener for the buttons 
+//is the button clicked correct or not
+//if correct one function for correct answers
+// one function for incorrect 
+
 
 
 startQuiz.onclick = start
